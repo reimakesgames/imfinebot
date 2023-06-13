12163f1b2a595678a8bae2b9827fd018a7a019d6
rei <reimakesgames.dev@gmail.com>
2023-06-13 21:02:28 +0800

    RAHHHHHHHHH

--- src/commands/general/getKeyFromRedis.ts
+++ src/commands/general/getKeyFromRedis.ts
@@ -1,15 +1,20 @@
-import { CommandInteraction, PermissionsBitField, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, PermissionsBitField, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
 import emojis from "../../data/emojis.json";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("horror")
-		.setDescription("Gets a key from redis"),
+		.setDescription("Gets a key from redis")
+		.addStringOption(new SlashCommandStringOption()
+			.setName("key")
+			.setDescription("The key to get")
+			.setRequired(true)
+		),
 	async execute(interaction) {
 		if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-			// const key = interaction.options.getString("key", true);
-			// const value = await interaction.redisClient.get("test");
-			await interaction.reply({ content: "test" });
+			const key = interaction.options.getString("key", true);
+			const value = await interaction.redisClient.get(key);
+			await interaction.reply({ content: value });
 		} else {
 			await interaction.reply({ content: `${emojis.default}` });
 		}
